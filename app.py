a35c5c0ae1d3154ea8516d7b415f1a53efcf9ad7
Ceralex <alessandroceresa05@gmail.com>
2024-04-13 09:01:08 +0200

    Modify homepage, add user balance

--- app.py
+++ app.py
@@ -14,21 +14,67 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 load_dotenv()
 
-# Database configuration
-DATABASE_URL = os.environ.get('DATABASE_URL')  # Get the database URL from environment variables
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 def check_password(db, username, password):
     cur = db.cursor()
-    cur.execute("SELECT password FROM users WHERE username = %s", (username,))
+    cur.execute("SELECT password FROM accounts WHERE username = %s", (username,))
     user = cur.fetchone()
     cur.close()
     if user is None:
         return False
     return user[0] == password
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+def get_balance_infos(db, username):
+    cur = db.cursor()
+    cur.execute("""SELECT
+                        accounts.username,
+                        SUM(COALESCE(transactions.amount, 0::money)) AS transaction_total,
+                        SUM(COALESCE(transfers_in.amount, 0::money)) AS received_transfers_total,
+                        SUM(COALESCE(transfers_out.amount, 0::money)) AS sent_transfers_total,
+                        SUM(COALESCE(transfers_in.amount, 0::money))
+                        - SUM(COALESCE(transactions.amount, 0::money))
+                        - SUM(COALESCE(transfers_out.amount, 0::money))::money AS balance
+                    FROM
+                        accounts
+                    LEFT JOIN transactions ON transactions.bank_account_id = accounts.id
+                    LEFT JOIN transfers transfers_in ON transfers_in.receiver_bank_account_id = accounts.id
+                    LEFT JOIN transfers transfers_out ON transfers_out.sender_bank_account_id = accounts.id
+                    WHERE
+                        accounts.username = %s
+                    GROUP BY
+                        accounts.username;""",
+      (username,))
+    infos = cur.fetchone()
+    cur.close()
+
+    total_transactions = infos[1]
+    total_received_transfers = infos[2]
+    total_sent_transfers = infos[3]
+    balance = infos[4]
+
+    return {
+        "total_transactions": total_transactions,
+        "total_received_transfers": total_received_transfers,
+        "total_sent_transfers": total_sent_transfers,
+        "balance": balance
+    }
+
+@app.get("/")
+def index():
+    return render_template('index.html')
+    
+@app.post("/login")
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    db = g.db
+    if check_password(db, username, password):
+        session["username"] = username
+        return redirect(url_for("me"))
+    else:
+        return render_template("index.html", error="Invalid username or password")
 
 @app.before_request
 def before_request():
@@ -45,33 +91,17 @@ def after_request(response):
 def me():
     if "username" in session:
         username = session["username"]
-        return render_template("private_page.html", username=username)
+
+        infos = get_balance_infos(g.db, username)
+        return render_template("me.html", username=username, infos=infos)
     else:
-        return redirect(url_for("login"))
+        return redirect(url_for("index"))
 
 @app.get("/logout")
 def logout():
     session.pop("username", None)
 
-    return redirect(url_for("login"))
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "GET":
-        return render_template('login.html')
-    else:
-        username = request.form["username"]
-        password = request.form["password"]
-
-        # password = hashlib.sha256(password.encode()).hexdigest()
-
-        db = g.db
-        if check_password(db,username, password):
-            session["username"] = username
-            return redirect(url_for("me"))
-        else:
-            return "<p>Wrong username or password</p>"
-
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
